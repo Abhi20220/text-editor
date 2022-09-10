da3732a9f219cba90f7a255289ad2373b20ed234
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-09-10 20:55:02 +0930

    install js update

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,25 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+  });
+  
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+    if (deferredPrompt !== null) {
+      await deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === "accepted") {
+        deferredPrompt = null;
+      }
+    }
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+    deferredPrompt = null;
+    console.log("Thank you for installing JATE Just Another Text Editor");
+  });
