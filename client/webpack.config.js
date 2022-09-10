cb119bfbb06fa9be1ad3d840115049ac453b36d7
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-09-10 20:48:22 +0930

    webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,22 +8,69 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        favicon: "./favicon.ico",
+        title: "Text Editor",
+      }),
+
+      
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+
+      new WebpackPwaManifest({
+        name: "JATE",
+        short_name: "JATE",
+        description: "Just another text editor!",
+        background_color: "#31a9e1",
+        theme_color: "#272822",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+        fingerprints: false
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
