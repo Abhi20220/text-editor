542ef2feb57b88e2ef2d6923233d60cfd42f9871
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-09-10 20:52:16 +0930

    database js update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Post to the database");
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  await store.clear()
+  const request = store.put({ text: content });
+  const result = await request;
+  console.log("text save to DB", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("Get from the database");
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  if(result.length < 1){
+    return null;
+  }
+  return result[0].text;
+};
 
 initdb();
